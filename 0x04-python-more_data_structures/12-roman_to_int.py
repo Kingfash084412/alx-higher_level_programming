fa1d10e1cd200db20a4eeab7453059e61c86b98a
Fashina Segun <fashina.segun25@gmail.com>
2022-09-28 21:00:28 +0100

    a function def roman_to_int(roman_string): that converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,30 +2,30 @@
 
 
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    i = 0
 
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-    }
-    num = 0
+    if (roman_string is None) or (type(roman_string) is not str):
+        return total
 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
+    while i in range(len(roman_string)):
 
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
+        if i == len(roman_string) - 1:
+            total += roman[roman_string[i]]
+            break
 
         else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+            if roman[roman_string[i]] < roman[roman_string[i+1]]:
+                total += roman[roman_string[i+1]] - roman[roman_string[i]]
+                i = i + 1
+
+                if i + 1 == len(roman_string):
+                    break
+
+            else:
+                total += roman[roman_string[i]]
+
+        i = i + 1
+
+    return total
