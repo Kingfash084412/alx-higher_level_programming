0130235628626a41492e001e3c7e4afa1d2888c8
Fashina Segun <fashina.segun25@gmail.com>
2022-10-10 17:54:54 -0700

    0x05. Python - Exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,6 +5,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except:
+    try/except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
