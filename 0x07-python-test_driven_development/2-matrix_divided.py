7f488c1033120ccab447c18239e9ffeb8dcf116a
Fashina Segun <fashina.segun25@gmail.com>
2022-10-19 16:55:20 +0100

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,36 +1,48 @@
 #!/usr/bin/python3
-"""Defines a matrix division function."""
+"""
+Function(s):
+    matrix_divided: Divides the contents of a matrix by div.
+"""
 
 
 def matrix_divided(matrix, div):
-    """Divide all elements of a matrix.
-
-    Args:
-        matrix (list): A list of lists of ints or floats.
-        div (int/float): The divisor.
-    Raises:
-        TypeError: If the matrix contains non-numbers.
-        TypeError: If the matrix contains rows of different sizes.
-        TypeError: If div is not an int or float.
-        ZeroDivisionError: If div is 0.
-    Returns:
-        A new matrix representing the result of the division.
     """
-    if (not isinstance(matrix, list) or matrix == [] or
-            not all(isinstance(row, list) for row in matrix) or
-            not all((isinstance(ele, int) or isinstance(ele, float))
-                    for ele in [num for row in matrix for num in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
-
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-
+    Recieves two values, checks if div is a number, and matrix only contains
+    number, then casts them as ints. Then all the the elements of the matrix
+    are divide by div, and a new matrix containing the result, rounded to
+    two decimal places, is returned.
+    Args:
+        matrix: Matrix of values.
+        div: Number to divide each item in matrix by.
+    Returns:
+        list: New matrix, each element divided by zero.
+    Raises:
+        TypeError: In the event of either variable
+            not being or containing an int or float.
+        ZeroDivisionError: In the event of an attempted division by zero.
+    Notes:
+        Instead of round, I came up with this math: int(t/div*100)/100)
+    """
+    matrixError = "matrix must be a matrix (list of lists) of integers/floats"
+    if not isinstance(matrix, list):
+        raise TypeError(matrixError)
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
-
-    if div == 0:
+    if div is 0:
         raise ZeroDivisionError("division by zero")
 
-    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
+    new_matrix = []
+    length = len(matrix[0])
+    for i in matrix:
+        hold = []
+        if not isinstance(i, list):
+            raise TypeError(matrixError)
+        if length != len(i):
+            raise TypeError("Each row of the matrix must have the same size")
+        for t in i:
+            if not isinstance(t, int) and not isinstance(t, float):
+                raise TypeError(matrixError)
+            hold.append(round(t/div, 2))
+        new_matrix.append(hold)
+    return new_matrix
 
